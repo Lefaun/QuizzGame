c6c743749538a3cdd781e0e719497042b98f7e8f
Lefaun <118660975+Lefaun@users.noreply.github.com>
2024-09-29 13:23:04 +0100

    Update postgres-quiz-server-form.js

--- postgres-quiz-server-form.js
+++ postgres-quiz-server-form.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+    origin: process.env.FRONTEND_URL || 'https://quizz-game-3q0f.onrender.com/'
 }));
 app.use(bodyParser.json());
 app.use(express.static('public')); // Serve static files from 'public'
